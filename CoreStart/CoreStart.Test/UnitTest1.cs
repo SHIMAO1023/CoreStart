9f5cc3693174d5444b76096e9eb4d3017f407f78
SHIMAO <shimao1023@outlook.com>
2019-03-04 11:03:39 +0800

    no message

---
+++ CoreStart/CoreStart.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoreStart.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
